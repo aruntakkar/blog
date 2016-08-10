6e98428a94508e8c6fa009b50ebfe8253f41a975
aruntakkar <aruntakkar5@gmail.com>
2016-08-10 23:56:54 +0530

    views | urls | templates/static

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 
 class PublishedManager(models.Manager):
@@ -30,6 +31,12 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-publish',)
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.strftime('%m'),
+                                                 self.publish.strftime('%d'),
+                                                 self.slug])
 
     def __str__(self):
         return self.title
--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,16 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
     # post views
     url(r'^$', views.post_list, name='post_list'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
-        r'^(?<post>[-\w]+)/$',
+        r'(?P<post>[-\w]+)/$',
         views.post_detail,
-        name='post_detail'),
+        name='post_detail')
+    # url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>d{2})/'\
+    #     r'^(?<post>[-\w]+)/$',
+    #     views.post_detail,
+    #     name='post_detail',
+    #     ),
 ]
