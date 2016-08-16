d725e593ce92970c710f8ac87f07faa207df132a
aruntakkar <aruntakkar5@gmail.com>
2016-08-16 19:42:46 +0530

    nothing modified

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     # url(r'^$', views.post_list, name='post_list'),
     url(r'^$', views.PostListView.as_view(), name='post_list'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
-        r'(?P<post>[-\w]+)/$',
-        views.post_detail,
-        name='post_detail'
-        ),
+    	r'(?P<post>[-\w]+)/$',
+    	views.post_detail,
+    	name='post_detail'
+    	),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,19 +4,19 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)  # 3 posts in each Page
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # if page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # if page is out of range deliver the last page of results
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 3)  # 3 posts in each Page
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         # if page is not an integer deliver the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # if page is out of range deliver the last page of results
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
 class PostListView(ListView):
